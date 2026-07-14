549ea06172261777ddced4dc30e98ac280709ebe
fmax39894-maker <fmax39894@gmail.com>
2026-07-14 07:24:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -16,17 +16,12 @@ onSnapshot
 
 const firebaseConfig={
 
-apiKey:"AIzaSyCo_kxcIJ6fqSMxflzwEvvHyFvnJ8jy8Zk",
-
-authDomain:"red-diamonds.firebaseapp.com",
-
-projectId:"red-diamonds",
-
-storageBucket:"red-diamonds.firebasestorage.app",
-
-messagingSenderId:"692631450885",
-
-appId:"1:692631450885:web:48615bc50d5b25ec6a336c"
+apiKey: "AIzaSyBBGeeIgjaPMhaUinuu2Gs3UCXt4P4Iazk",
+  authDomain: "red-diamonds-3fc24.firebaseapp.com",
+  projectId: "red-diamonds-3fc24",
+  storageBucket: "red-diamonds-3fc24.firebasestorage.app",
+  messagingSenderId: "377556759682",
+  appId: "1:377556759682:web:31f1a4b7cb6402268bf09b",
 
 };
 
